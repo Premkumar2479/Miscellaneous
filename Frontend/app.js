5edbd0c5764d6cf01ac33fe8021c0e099a9ad4d7
Premkumar2479 <pk2335301@gmail.com>
2025-10-11 11:11:52 +0530

    some text

---
+++ Frontend/app.js
@@ -0,0 +1,15 @@
+const stu1 ={
+    name:"john",
+    age: 23,
+    marks: 56,
+}
+const stu2 ={
+    name:"john",
+    age: 23,
+    marks: 56,
+}
+const stu3 ={
+    name:"john",
+    age: 23,
+    marks: 56,
+}
