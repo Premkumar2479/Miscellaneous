96261e3e0cb88625584390ad154adef7753590b7
Premkumar2479 <pk2335301@gmail.com>
2025-10-21 14:05:00 +0530

    some message

--- Frontend/app.js
+++ Frontend/app.js
@@ -2,14 +2,25 @@ const stu1 ={
     name:"john",
     age: 23,
     marks: 56,
-}
+    getMarks:function() {
+        return this.marks;
+    },
+};
+
 const stu2 ={
-    name:"john",
+    name:"joe",
     age: 23,
-    marks: 56,
-}
+    marks: 99,
+    getMarks:function() {
+        return this.marks;
+    },
+};
+
 const stu3 ={
-    name:"john",
+    name:"jerry",
     age: 23,
-    marks: 56,
-}
+    marks: 88,
+    getMarks:function() {
+        return this.marks;
+    },
+};
